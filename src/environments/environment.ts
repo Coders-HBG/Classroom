b1c75d211d589dc810a4130cfff18257356bc14f
Max Carlquist <tenkaklet@gmail.com>
2022-07-26 14:37:46 +0200

    Introduced basic structure

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'classroom-cb5c1',
+    appId: '1:53849865808:web:0c15b9a28e210467fe9b55',
+    storageBucket: 'classroom-cb5c1.appspot.com',
+    apiKey: 'AIzaSyByBSu6UfAmUkINX6icNOBDQ0m2NXlWowk',
+    authDomain: 'classroom-cb5c1.firebaseapp.com',
+    messagingSenderId: '53849865808',
+    measurementId: 'G-5QB79LS97X',
+  },
   production: false
 };
 
